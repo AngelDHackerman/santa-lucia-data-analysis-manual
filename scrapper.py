e33129072fd9344dfe16d8342b54e3433b485d67
Angel Hernandez <angeldariaux@gmail.com>
2024-12-01 20:10:46 -0600

    created code for see in the text of the body with all the numbers and combinations

--- scrapper.py
+++ scrapper.py
@@ -23,12 +23,29 @@ try:
     
     # Wait for the page to load and close add
     close_ad = wait.until(EC.visibility_of_element_located((By.ID, "ocultarAnuncio"))) # //*[@id="ocultarAnuncio"]
+    # click on the "close button" using javascript
     driver.execute_script("arguments[0].click();", close_ad)
     
     # Click on the lottery number
     element = wait.until(EC.presence_of_element_located((By.XPATH, f"//a[contains(@href, 'id={lotter_number}')]")))
     driver.execute_script("arguments[0].click();", element)
     time.sleep(10)
+    
+    # Look for the header
+    header = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "heading_s1.text-center")))
+    # Extract the text information for the winner numbers, date, lotter number and refunds
+    header_text = header.text
+    print(header_text)
+    
+    # Look for the body
+    body_content = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "(//div[@class='card-body']//div[@class='row'])[3]") # # Third 'row' inside 'card-body'
+    ))
+    # Extract the information for he body card with all the winner numbers, combinations and total prizes 
+    body_results = body_content.text
+    print(body_results)
+    
 finally:
     # Close Browser
     driver.quit()
+    
