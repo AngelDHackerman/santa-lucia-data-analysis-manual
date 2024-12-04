2b4cd9839d1c8224bba1971def649e25aa883346
Angel Hernandez <angeldariaux@gmail.com>
2024-12-03 20:41:08 -0600

    downloaded files from id 181 to 205 (also the one with id 177) to raw text

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-lottery_number = 177
+lottery_number = 205
 
 # Setup for ChromeDriver
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
