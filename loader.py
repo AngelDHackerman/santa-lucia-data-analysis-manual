21c397f7bc75e30f28d6b2b423c006ff10123608
Angel Hernandez <angeldariaux@gmail.com>
2024-12-08 20:02:59 -0600

    moved show_tables_ to AWS, renamed the scrapper, removed .mode() operations from the transformer, added files for the loader and the main.py

---
+++ loader.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+import pymysql
+
+rds_client = boto3.client('rds')
+
+# Get the secret from AWS Secrets Manager
+def get_secret():
+
+    secret_name = "LotteryDBCredentials"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Parse the secret string into a dictionary
+    secret = json.loads(get_secret_value_response['SecretString'])
+    return secret['username'], secret['password']
